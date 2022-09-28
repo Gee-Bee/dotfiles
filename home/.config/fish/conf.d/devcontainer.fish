990cacbb2bd2d2c2cf28cc6999c6edf40d88245d
Gee-Bee <greg.bunia@gmail.com>
2022-09-28 17:53:52 +0200

    devdontainer.fish

---
+++ home/.config/fish/conf.d/devcontainer.fish
@@ -0,0 +1,3 @@
+if [ -d $HOME/.config/Code/User/globalStorage/ms-vscode-remote.remote-containers/cli-bin ]
+  fish_add_path $HOME/.config/Code/User/globalStorage/ms-vscode-remote.remote-containers/cli-bin
+end
