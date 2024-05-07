cad0c596e7c37cce34ff4098839c40b50ae65676
Gee-Bee <greg.bunia@gmail.com>
2024-05-07 14:26:26 +0200

    chore: starship.fish config

--- home/.config/fish/conf.d/podman.fish
+++ home/.config/fish/conf.d/podman.fish
@@ -1,4 +1,4 @@
 if type -qs podman
   systemctl start --user podman.socket
-  export DOCKER_HOST="unix://$XDG_RUNTIME_DIR/podman/podman.sock"
+  export DOCKER_HOST="unix:/$XDG_RUNTIME_DIR/podman/podman.sock"
 end
