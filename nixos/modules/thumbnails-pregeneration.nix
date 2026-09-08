a5b93e28d2b737a1242ccd88f5e50d9d90e73d12
Gee-Bee <greg.bunia@gmail.com>
2026-09-08 14:40:49 +0200

    chore(nixos): simplify thumbnail-pregeneration

--- nixos/modules/thumbnails-pregeneration.nix
+++ nixos/modules/thumbnails-pregeneration.nix
@@ -5,22 +5,19 @@ let
     #!${pkgs.fish}/bin/fish
 
     set DIRS "$HOME/Downloads" "$HOME/Pictures/Screenshots"
+    mkdir -p $DIRS
 
-    # Szybka inicjalizacja katalogów bazowych
-    for d in $DIRS; test -d $d; or mkdir -p $d; end
-
-    # Monitorowanie zdarzeń I/O i natychmiastowe delegowanie zadań do silnika Plasma 6
     ${pkgs.inotify-tools}/bin/inotifywait -q -m -e close_write --format "%w%f" $DIRS | while read -l f
-      if string match -r -i '\.(jpg|jpeg|png|webp|gif)$' "$f"
-        qdbus org.freedesktop.thumbnails.Manager1 /org/freedesktop/thumbnails/Manager1 \
-          Queue "file://$(path resolve $f)" "" "large" "default" 0 >/dev/null 2>&1
-      end
+      string match -qr -i '\.(jpg|jpeg|png|webp|gif)$' "$f"; and \
+        ${pkgs.kdePackages.qttools}/bin/qdbus org.freedesktop.thumbnails.Manager1 \
+          /org/freedesktop/thumbnails/Manager1 Queue \
+          "file://$(path resolve $f)" "" "large" "default" 0 >/dev/null 2>&1
     end
   '';
 in
 {
   systemd.user.services.preemptive-thumbnails = {
-    description = "Pregenerowanie miniatur graficznych w tle dla Downloads i Screenshots";
+    description = "Pregenerate image thumbnails for Downloads and Screenshots";
 
     wantedBy = [ "graphical-session.target" ];
     partOf = [ "graphical-session.target" ];
